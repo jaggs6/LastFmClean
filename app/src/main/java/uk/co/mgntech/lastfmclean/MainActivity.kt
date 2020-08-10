0e3747a5ac025355ffca45bf28261ff8ee834d58
Jagdeep Nagpal <jagdeepnagpal@gmail.com>
2020-08-10 01:13:12 +0100

    init

---
+++ app/src/main/java/uk/co/mgntech/lastfmclean/MainActivity.kt
@@ -0,0 +1,20 @@
+package uk.co.mgntech.lastfmclean
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import uk.co.mgntech.lastfmclean.ui.main.SectionsPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+    }
+}
