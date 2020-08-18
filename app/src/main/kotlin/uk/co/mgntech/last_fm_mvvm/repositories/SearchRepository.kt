1d3b4fbb481128336da68bf031c1d8d4e73d3d8a
Jagdeep Nagpal <jagdeepnagpal@gmail.com>
2020-08-18 22:58:17 +0100

    clean up

--- app/src/main/kotlin/uk/co/mgntech/last_fm_mvvm/repositories/SearchRepository.kt
+++ app/src/main/kotlin/uk/co/mgntech/last_fm_mvvm/repositories/SearchRepository.kt
@@ -1,27 +1,25 @@
 package uk.co.mgntech.last_fm_mvvm.repositories
 
-import androidx.lifecycle.LiveData
-import uk.co.mgntech.last_fm_mvvm.models.Search
 import uk.co.mgntech.last_fm_mvvm.models.SearchType
 import uk.co.mgntech.last_fm_mvvm.requests.SearchApiClient
 
 class SearchRepository {
 
+    companion object {
+        val instance = SearchRepository()
+    }
+
     private val _apiClient = SearchApiClient.instance
 
-    fun artistsLoading(): LiveData<Boolean> = _apiClient.artistsLoading
-    fun albumsLoading(): LiveData<Boolean> = _apiClient.albumsLoading
-    fun songsLoading(): LiveData<Boolean> = _apiClient.songsLoading
+    fun artistsLoading() = _apiClient.artistsLoading
+    fun albumsLoading() = _apiClient.albumsLoading
+    fun songsLoading() = _apiClient.songsLoading
 
-    fun albums(): LiveData<List<Search>> = _apiClient.albums
-    fun songs(): LiveData<List<Search>> = _apiClient.songs
-    fun artists(): LiveData<List<Search>> = _apiClient.artists
+    fun albums() = _apiClient.albums
+    fun songs() = _apiClient.songs
+    fun artists() = _apiClient.artists
 
     fun searchApi(query: String, type: SearchType, pageNumber: Int) {
         _apiClient.searchApi(query, type, pageNumber)
-    }
-
-    companion object {
-        val instance = SearchRepository()
     }
 }
